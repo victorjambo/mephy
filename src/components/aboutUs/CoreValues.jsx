3a72f45682c23d4aede15f85a0b0e38c5eedc203
victorjambo <victorjambo@live.com>
2019-06-08 16:51:07 +0300

    core values

---
+++ src/components/aboutUs/CoreValues.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const CoreValues = () => (
+  <section className="bg_white flat_white">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="title_area">
+            <h3 className="section_title text-center down_line_center">Our Core Values</h3>
+            <span className="sub_title text-center">Caring for the growing needs of our community</span>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">H</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Honesty</div></h6>
+            <p>At Mephys we are true to our patients and ourselves.</p>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">E</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Enjoyment</div></h6>
+            <p>Mephys promotes FUN in everything we do. Life is here to be enjoyed; we love coming to work each day and aim to create an enjoyable space where you are encouraged to succeed.</p>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">A</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Attitude</div></h6>
+            <p>At Mephys we understand the benefits of a positive attitude towards creating an inspirational and motivational atmosphere to improving patient centered outcomes.</p>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">L</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Leadership</div></h6>
+            <p>Mephys are leaders in the health and wellness industry. We pride ourselves on earning the customer’s trust through effective leadership, leading to high achievement. Within the company, leadership from our staff members defines Mephy's business model and contributes significantly to its success.</p>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">T</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Teamwork</div></h6>
+            <p>Mephys understand a successful team motivates to reach desired outcomes. Team refers to family, friends, sporting groups, workplaces and health service providers (Mephys staff), which all play a pivotal role in success. We work together, laugh together, exercise together and most importantly, grow together.</p>
+          </div>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <div className="text_block_4 mb_30">
+            <span className="letter-icon">H</span>
+            <h6 className="inner_title mb_10"><div className="color_secondary">Health</div></h6>
+            <p>We practice what we preach. We look after our body; eat well, train hard and live life to</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default CoreValues;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../common/Banner';
 import MissionVision from '../components/aboutUs/MissionVision';
 import Introduction from '../components/aboutUs/Introduction';
+import CoreValues from '../components/aboutUs/CoreValues';
 
 const AboutUs = () => (
   <React.Fragment>
     <Banner pageTitle="About us" titlePreviousPageLink="Home" previousPageLink="/" titleCurrentPage="About us" />
     <Introduction />
     <MissionVision />
+    <CoreValues />
   </React.Fragment>
 );
 
